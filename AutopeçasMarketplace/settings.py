274ff5299c2aade8a166cc0f647aad2c47543ea9
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-10-27 19:59:16 -0300

    feature: ajust no requimentes e no settings

--- AutopeçasMarketplace/settings.py
+++ AutopeçasMarketplace/settings.py
@@ -29,12 +29,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 
 DEV = config("DEV", default=False, cast=bool)
 
